9631e39b97f250633defe08db62f674a88497459
YaBoiSkinnyP <jadenrodriguez7@gmail.com>
2021-07-14 01:53:36 -0500

    started test suite

---
+++ test_snake_game.py
@@ -0,0 +1,41 @@
+import unittest
+#from snake_game import 
+
+class TestSnakeGUI(unittest.TestCase):
+    pass
+
+class TestGameSelectorFrame(unittest.TestCase):
+    pass
+
+class TestGameOverFrame(unittest.TestCase):
+    pass
+
+class TestClassicGame(unittest.TestCase):
+    pass
+
+class TestEnemyClassicGame(unittest.TestCase):
+    pass
+
+class TestTwoPlayerGame(unittest.TestCase):
+    pass
+
+class TestCompetitiveGame(unittest.TestCase):
+    pass
+
+class TestGameObj(unittest.TestCase):
+    pass
+
+class TestSnake(unittest.TestCase):
+    pass
+
+class TestMainPlayer(unittest.TestCase):
+    pass
+
+class TestEnemy(unittest.TestCase):
+    pass
+
+class TestFood(unittest.TestCase):
+    pass
+
+class TestPlayer2(unittest.TestCase):
+    pass
